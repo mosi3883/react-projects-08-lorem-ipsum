ddfc2ef97a3e186018d1370405a301991dfc1b1b
Mostafa <mosi3883@gmail.com>
2022-05-06 14:10:19 +0430

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
+import shuffle from 'underscore/modules/shuffle';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+  const maxCount = data.length;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount < 0) {
+      amount = 0;
+    }
+    if (amount > maxCount) {
+      amount = maxCount;
+    }
+    setText(shuffle(data).slice(0, amount));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          min='0'
+          max={maxCount}
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
 }
 
 export default App;
